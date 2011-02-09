46f47ccb261223387e1b49376cdd9d37a6f158d5
Aziz Light <aziiz.light+github@gmail.com>
2011-02-09 21:57:17 +0100

    Title, date and extension validations. Slug generation. Options retrieval. And associated tests.

--- lib/jepeto/jekyll_post.rb
+++ lib/jepeto/jekyll_post.rb
@@ -1,14 +1,34 @@
 module Jepeto
 
   class JekyllPost
+    attr_reader :options
+
     def initialize(options)
-      if options[:title].nil? || options[:title].empty?
-        raise ArgumentError, 'invalid argument passed (valid title required)'
+      # The title, the date and the extension are the only options to be
+      # mandatory! Without them, the post file's name can't be generated.
+      %w[title date extension].each do |option|
+        if options[option.to_sym].nil? || options[option.to_sym].empty?
+          raise ArgumentError, "invalid argument passed (valid #{option} required)"
+        end
       end
 
       @options = options
+
+      generate!
     end
 
+    def slug
+      @options[:slug].nil? ? generate_slug : @options[:slug]
+    end
 
+    private
+
+    def generate!
+      generate_slug
+    end
+
+    def generate_slug
+      @options[:slug] = @options[:title].downcase.strip.gsub(/[ _\.-]+/, '-')
+    end
   end
 end
--- test/test_jekyll_post.rb
+++ test/test_jekyll_post.rb
@@ -4,7 +4,54 @@ require "date"
 class TestJekyllPost < MiniTest::Unit::TestCase
 
   def setup
-    @options = {
+    @options = default_options
+    @post = Jepeto::JekyllPost.new(@options)
+  end
+
+  def test_title_date_and_extension_should_be_valid
+    %w[title date extension].each do |option|
+      @options[option.to_sym] = ''
+      assert_raises(ArgumentError) { Jepeto::JekyllPost.new(@options) }
+
+      # Reset the options hash to be certain that the test fails for
+      # the right reasons!
+      @options = default_options
+
+      @options[option.to_sym] = nil
+      assert_raises(ArgumentError) { Jepeto::JekyllPost.new(@options) }
+
+      # Reset the options hash so that other tests are not affected!
+      @options = default_options
+    end
+  end
+
+  def test_post_options_should_be_retrievable
+    assert_kind_of Hash, @post.options
+    refute_empty @post.options
+  end
+
+  def test_slug_should_be_generated
+    refute_nil @post.options[:slug]
+  end
+
+  def test_slug_should_be_retrievable
+    assert_equal @post.slug, @post.options[:slug]
+  end
+
+  def test_slug_should_have_the_right_format
+    assert_match /^(?:[a-z0-9]+)(?:-[a-z0-9]+)*$/, @post.slug
+  end
+
+
+  # def test_filename_should_have_the_right_format
+  #   assert_equal @post.filename, "#{@options[:date]}-#{@options[:slug]}.#{@options[:extension]}"
+  # end
+
+  private
+
+  # This method can be used to reset the options hash.
+  def default_options
+    {
       layout:     'default',
       title:      'The Title',
       extension:  'markdown',
@@ -12,15 +59,4 @@ class TestJekyllPost < MiniTest::Unit::TestCase
       date:       Date.today.to_s
     }
   end
-
-  def test_title_should_not_be_empty
-    @options[:title] = ''
-    assert_raises(ArgumentError) { Jepeto::JekyllPost.new(@options) }
-  end
-
-  def test_title_should_not_be_nil
-    @options[:title] = nil
-    assert_raises(ArgumentError) { Jepeto::JekyllPost.new(@options) }
-  end
-
 end
