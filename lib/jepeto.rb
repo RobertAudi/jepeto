dfb674e09f38082fe626ada80854d6596eb9cb05
Aziz Light <aziiz.light+github@gmail.com>
2011-02-26 11:59:17 +0100

    Real version 0.1

--- lib/jepeto.rb
+++ lib/jepeto.rb
@@ -3,9 +3,6 @@ require "date"
 require "yaml"
 require "optparse"
 
-# Debug Shite
-require "pp"
-
 # Personal requirements
 require_relative "./jepeto/version"
 require_relative "./jepeto/jekyll_post"
