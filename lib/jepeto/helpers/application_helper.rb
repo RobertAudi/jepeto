f1e00930e614c434115aba0bfd51f04ed1fa77bc
Aziz Light <aziz@azizlight.me>
2012-02-04 17:49:43 +0000

    Fixed a bug in the application helper

--- lib/jepeto/helpers/application_helper.rb
+++ lib/jepeto/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module Jepeto
   module ApplicationHelper
     def slugalize(string)
-      string.gsub!(/[^\x00-\x7F]+/, '')   # Remove non-ASCII (e.g. diacritics).
+      string = string.dup
+      string.gsub!(/[^\x00-\x7F]+/, '')     # Remove non-ASCII (e.g. diacritics).
       string.gsub!(/[^a-z0-9\-_\+]+/i, '-') # Turn non-slug chars into the separator.
       string.gsub!(/-{2,}/, '-')            # No more than one of the separator in a row.
-      string.gsub!(/^-|-$/, '')           # Remove leading/trailing separator.
+      string.gsub!(/^-|-$/, '')             # Remove leading/trailing separator.
       string.downcase!
       string
     end
